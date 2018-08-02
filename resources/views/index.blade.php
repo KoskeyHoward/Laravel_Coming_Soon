145892ff547b04ec9bcfca7f5bf40ac944164ab4
Le-Yo <koskeyhoward@gmail.com>
2018-08-02 12:34:43 +0300

    further changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -116,7 +116,7 @@
 
     <div class="container">
         <h1 class="logo cursive">
-            Cido South Sudan
+            Civian Software Solutions
         </h1>
 <!--  H1 can have 2 designs: "logo" and "logo cursive"           -->
         
